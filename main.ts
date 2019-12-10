9a92a9373bdce401ff31cee9f0e032e751ad4479
carllee1119 <58464766+carllee1119@users.noreply.github.com>
2019-12-10 17:13:36 +0800

    test

--- main.ts
+++ main.ts
@@ -264,7 +264,7 @@ namespace GHBit {
         }               
     }
     
-/*    //% blockId=GHBit_Rocker block="Rocker|value %value"	*/
+
     //% blockId=GHBit_Rocker block="JoyStick Direction|value %value"
     //% weight=96
     //% blockGap=10
